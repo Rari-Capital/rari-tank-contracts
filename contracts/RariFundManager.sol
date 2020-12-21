e6d86070ab468d8100854aded2fef3c8e1dc3c95
Jet <jetjadeja@gmail.com>
2020-12-21 12:54:08 -0800

    🔨 Refactored FundManager and CPC

--- contracts/RariFundManager.sol
+++ contracts/RariFundManager.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.5.0;
 
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/token/erc20/IERC20.sol";
+import "@openzeppelin/contracts/token/erc20/SafeERC20.sol";
+
 import "./RariFundController.sol";
 
 /**
@@ -7,9 +11,15 @@ import "./RariFundController.sol";
     @notice Handles deposits and withdrawals into the pool
     @author Jet Jadeja (jet@rari.capital)
 */
-contract RariFundManager {
+contract RariFundManager is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
     ///@dev The address of the RariFundController Contract
     address private rariFundControllerContract;
+
+    ///@dev The RariFundController contract
+    RariFundController private rariFundController;
 
     ///@dev An array of supported currencies
     string[] private supportedCurrencies;
@@ -23,7 +33,16 @@ contract RariFundManager {
     ///@dev Maps supported currency codes to their decimal precisions
     mapping(string => uint256) private currencyDecimals;
 
-    constructor() public {
+    ///@dev Ensures that a function can only be called from the RariFundController
+    modifier onlyController() {
+        require(
+            msg.sender == rariFundControllerContract,
+            "RariFundManager: Function must be called by the Fund Controller"
+        );
+        _;
+    }
+
+    constructor(address _rariFundControllerContract) public Ownable() {
         addSupportedCurrency("BAT", 0x0D8775F648430679A709E98d2b0Cb6250d2887EF, 18);
         addSupportedCurrency("COMP", 0xc00e94Cb662C3520282E6f5717214004A7f26888, 18);
         addSupportedCurrency("WBTC", 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599, 8);
@@ -31,18 +50,36 @@ contract RariFundManager {
         addSupportedCurrency("USDC", 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 6);
         addSupportedCurrency("USDT", 0xdAC17F958D2ee523a2206206994597C13D831ec7, 6);
         addSupportedCurrency("ZRX", 0xE41d2489571d322189246DaFA5ebDe1F4699F498, 18);
+
+        rariFundControllerContract = _rariFundControllerContract;
+        rariFundController = RariFundController(_rariFundControllerContract);
     }
 
-    ///@dev Add a supported ERC20 token to the contract
+    /**
+        @dev Add a supported ERC20 token to the contract
+        @param currencyCode The currency code of the token
+        @param currencyAddress The ERC20 contract address of the token
+        @param decimals The decimal presion of the token
+    */
     function addSupportedCurrency(
-        string memory _currencyCode,
-        address _currencyAddress,
-        uint256 _decimals
+        string memory currencyCode,
+        address currencyAddress,
+        uint256 decimals
     ) internal {
-        currencyIndexes[_currencyCode] = supportedCurrencies.length;
-        supportedCurrencies.push(_currencyCode);
+        currencyIndexes[currencyCode] = supportedCurrencies.length;
+        supportedCurrencies.push(currencyCode);
+        currencyAddresses[currencyCode] = currencyAddress;
+        currencyDecimals[currencyCode] = decimals;
+    }
 
-        currencyAddresses[_currencyCode] = _currencyAddress;
-        currencyDecimals[_currencyCode] = _decimals;
+    /**
+        @dev Deposit funds into the contract and send them to the FundController
+        @param currencyCode The currency code of the asset 
+        @param amount The amount of the asset to be deposited
+    */
+    function deposit(string calldata currencyCode, uint256 amount) external {
+        address erc20TokenContract = currencyAddresses[currencyCode];
+        //prettier-ignore
+        require(erc20TokenContract != address(0), "RariFundManager: Invalid Currency Code");
     }
 }
--- contracts/libraries/CompoundPoolController.sol
+++ contracts/libraries/CompoundPoolController.sol
@@ -10,37 +10,35 @@ import "@openzeppelin/contracts/token/erc20/SafeERC20.sol";
 
 /**
     @title CompoundPoolController
-    @author David Lucid <david@rari.capital>, Jet Jadeja (jet@rari.capital)
+    @author David Lucid (david@rari.capital), Jet Jadeja (jet@rari.capital)
     @dev This library is used to handle interactions with Compound
 */
 library CompoundPoolController {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-
     /**
         @dev Deposits collateral into Compound
-        @param _underlying The address of the underlying asset being supplied
-        @param _amount The amount of the asset being supplied
-        @param _comptroller The address of Compound's Comptroller
+        @param erc20Contract The address of the underlying asset being supplied
+        @param amount The amount of the asset being supplied
+        @param comptrollerContract The address of Compound's Comptroller
     */
     function deposit(
-        address _underlying,
-        uint256 _amount,
-        address _comptroller
+        address erc20Contract,
+        uint256 amount,
+        address comptrollerContract
     ) external {
-        require(_amount > 0, "CompoundPoolController: Amount must be greater than 0");
+        require(amount > 0, "CompoundPoolController: Amount must be greater than 0");
 
-        address cErc20Contract = getCErc20Contract(_underlying);
-        IERC20 underlying = IERC20(_underlying);
+        address cErc20Contract = getCErc20Contract(erc20Contract);
         CErc20 cToken = CErc20(cErc20Contract);
-        Comptroller comptroller = Comptroller(_comptroller);
+        Comptroller comptroller = Comptroller(comptrollerContract);
 
         // Approve the transfer of the underlying token as collateral
-        underlying.approve(cErc20Contract, _amount);
+        IERC20(erc20Contract).approve(cErc20Contract, amount);
 
         // Mint cTokens in return for the underlying assset
-        uint256 error = cToken.mint(_amount);
+        uint256 error = cToken.mint(amount);
         require(error == 0, "CERC20: Mint Error");
 
         // Enter markets
@@ -51,78 +49,74 @@ library CompoundPoolController {
         require(errors[0] == 0, "Comptroller: Failed to enter markets");
     }
 
+
     /**
         @dev Borrows a certain amount of an asset from Compound
-        @param _underlying The address of the underlying asset being borrowed
-        @param _amount The amount of the underlying asset being supplied
+        @param erc20Contract The address of the underlying asset being borrowed
+        @param amount The amount of the underlying asset being supplied
     */
-    function borrow(address _underlying, uint256 _amount) external {
-        require(_amount > 0, "CompoundPoolController: Amount must be greater than 0");
-
-        address cErc20Contract = getCErc20Contract(_underlying);
-        CErc20 cToken = CErc20(cErc20Contract);
+    function borrow(address erc20Contract, uint256 amount) external {
+        require(amount > 0, "CompoundPoolController: Amount must be greater than 0");
 
         //Borrow Tokens
-        cToken.borrow(_amount);
+        CErc20(getCErc20Contract(erc20Contract)).borrow(amount);
     }
 
     /**
-        @dev Get the maximum borrow amount for a specific currency
-        @param _underlyingCollateral The address of the underlying token that serves as collateral
-        @param _amount The amount of collateral that the user has deposited
-        @param _underlyingBorrow The address of the token to be borrowed
-        @param _comptroller The address of Compound's comptroller
-        @param _priceFeed The address of Compound's price feed
+        @dev Get the maximum borrow amount in USD
+        @param erc20Contract The address of underlying ERC20 Contract
+        @param amount The amount 
     */
-    function getMaxUnderlyingBorrowAmount(
-        address _underlyingCollateral,
-        uint256 _amount,
-        address _underlyingBorrow,
-        address _comptroller,
-        address _priceFeed
-    ) public returns (uint256) {
-        address collateralCErc20Contract = getCErc20Contract(_underlyingCollateral);
-        CErc20 collateral = CErc20(collateralCErc20Contract);
-        CErc20 borrowing = CErc20(getCErc20Contract(_underlyingBorrow));
-        Comptroller comptroller = Comptroller(_comptroller);
-        PriceFeed priceFeed = PriceFeed(_priceFeed);
+    function getMaxUSDBorrowAmount(
+        address erc20Contract, 
+        uint256 amount, 
+        address comptrollerContract,
+        address priceFeedContract
+    ) external returns (uint256) {
+        address cErc20Contract = getCErc20Contract(erc20Contract);
+        CErc20 cToken = CErc20(cErc20Contract);
+        Comptroller comptroller = Comptroller(comptrollerContract);
+        PriceFeed priceFeed = PriceFeed(priceFeedContract);
+        
+        // Get the collateral factor of the asset
+        (, uint256 collateralFactorMantissa) = comptroller.markets(cErc20Contract);
 
-        // Get the collateral factor of the collateralized asset
-        (, uint256 collateralFactorMantissa) =
-            comptroller.markets(collateralCErc20Contract);
-
-        // Calculate the USD Balance of the user
-        uint256 underlyingBalanceMantissa = _amount
-            .mul(collateral.exchangeRateCurrent())
+        // Calculate the USD Amount
+        uint256 underlyingBalanceMantissa = amount
+            .mul(cToken.exchangeRateCurrent())
             .div(1e18);
 
         uint256 usdBalanceMantissa = underlyingBalanceMantissa
-            .mul(priceFeed.getUnderlyingPrice(collateralCErc20Contract))
+            .mul(priceFeed.getUnderlyingPrice(cErc20Contract))
             .div(1e18);
 
-        // Calculate the total USD borrow amount
-        uint256 usdBorrowAmountMantissa = usdBalanceMantissa
+        // Calculate and return the total USD borrow amount
+        return usdBalanceMantissa
             .mul(collateralFactorMantissa)
             .div(1e18);
+    }
 
-        // Return the total borrow supply for the asset
-        return usdBorrowAmountMantissa
-            .mul(priceFeed.getUnderlyingPrice(getCErc20Contract(_underlyingBorrow)))
-            .div(1e18);
+    /**
+        @dev Use the exchange rate to convert from Erc20 to CErc20
+        @param erc20Contract The address of the underlying ERC20 contract
+        @param amount The amount of underlying tokens
+     */
+    function getUnderlyingToCTokens(address erc20Contract, uint256 amount) external returns (uint256) {
+        return amount.mul(1e18).div(CErc20(erc20Contract).exchangeRateCurrent());
     }
 
     /**
         @dev Returns a token's cToken contract address given its ERC20 contract address.
-        @param _erc20Contract The ERC20 contract address of the token
+        @param erc20Contract The ERC20 contract address of the token
     */
-    function getCErc20Contract(address _erc20Contract) private pure returns (address) {
-        if (_erc20Contract == 0x0D8775F648430679A709E98d2b0Cb6250d2887EF) return 0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E; // BAT => cBAT
-        if (_erc20Contract == 0xc00e94Cb662C3520282E6f5717214004A7f26888) return 0x70e36f6BF80a52b3B46b3aF8e106CC0ed743E8e4; // COMP => cCOMP
-        if (_erc20Contract == 0x6B175474E89094C44Da98b954EedeAC495271d0F) return 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643; // DAI => cDAI
-        if (_erc20Contract == 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984) return 0x35A18000230DA775CAc24873d00Ff85BccdeD550; // UNI => cUNI
-        if (_erc20Contract == 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48) return 0x39AA39c021dfbaE8faC545936693aC917d5E7563; // USDC => cUSDC
-        if (_erc20Contract == 0xdAC17F958D2ee523a2206206994597C13D831ec7) return 0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9; // USDT => cUSDT
-        if (_erc20Contract == 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599) return 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4; // WBTC => cWBTC
+    function getCErc20Contract(address erc20Contract) private pure returns (address) {
+        if (erc20Contract == 0x0D8775F648430679A709E98d2b0Cb6250d2887EF) return 0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E; // BAT => cBAT
+        if (erc20Contract == 0xc00e94Cb662C3520282E6f5717214004A7f26888) return 0x70e36f6BF80a52b3B46b3aF8e106CC0ed743E8e4; // COMP => cCOMP
+        if (erc20Contract == 0x6B175474E89094C44Da98b954EedeAC495271d0F) return 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643; // DAI => cDAI
+        if (erc20Contract == 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984) return 0x35A18000230DA775CAc24873d00Ff85BccdeD550; // UNI => cUNI
+        if (erc20Contract == 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48) return 0x39AA39c021dfbaE8faC545936693aC917d5E7563; // USDC => cUSDC
+        if (erc20Contract == 0xdAC17F958D2ee523a2206206994597C13D831ec7) return 0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9; // USDT => cUSDT
+        if (erc20Contract == 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599) return 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4; // WBTC => cWBTC
         else revert("CompoundFundController: Supported Compound cToken address not found for this token address");
     }
 }
