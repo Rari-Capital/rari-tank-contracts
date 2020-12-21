c384e6c60a6637bf79b254a7b87a786d44324155
Jet <jetjadeja@gmail.com>
2020-12-20 16:16:05 -0800

    💚  Added max borrow amount function

--- contracts/libraries/CompoundPoolController.sol
+++ contracts/libraries/CompoundPoolController.sol
@@ -4,6 +4,7 @@ import "../external/compound/CErc20.sol";
 import "../external/compound/Comptroller.sol";
 import "../external/compound/Pricefeed.sol";
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/erc20/IERC20.sol";
 import "@openzeppelin/contracts/token/erc20/SafeERC20.sol";
 
@@ -13,33 +14,21 @@ import "@openzeppelin/contracts/token/erc20/SafeERC20.sol";
     @dev This library is used to handle interactions with Compound
 */
 library CompoundPoolController {
+    using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    /**
-        @dev Returns a token's cToken contract address given its ERC20 contract address.
-        @param _erc20Contract The ERC20 contract address of the token
-    */
-    function getCErc20Contract(address _erc20Contract) private pure returns (address) {
-        if (_erc20Contract == 0x0D8775F648430679A709E98d2b0Cb6250d2887EF) return 0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E; // BAT => cBAT
-        if (_erc20Contract == 0xc00e94Cb662C3520282E6f5717214004A7f26888) return 0x70e36f6BF80a52b3B46b3aF8e106CC0ed743E8e4; // COMP => cCOMP
-        if (_erc20Contract == 0x6B175474E89094C44Da98b954EedeAC495271d0F) return 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643; // DAI => cDAI
-        if (_erc20Contract == 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984) return 0x35A18000230DA775CAc24873d00Ff85BccdeD550; // UNI => cUNI
-        if (_erc20Contract == 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48) return 0x39AA39c021dfbaE8faC545936693aC917d5E7563; // USDC => cUSDC
-        if (_erc20Contract == 0xdAC17F958D2ee523a2206206994597C13D831ec7) return 0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9; // USDT => cUSDT
-        if (_erc20Contract == 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599) return 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4; // WBTC => cWBTC
-        else revert("CompoundFundController: Supported Compound cToken address not found for this token address");
-    }
 
     /**
         @dev Deposits collateral into Compound
         @param _underlying The address of the underlying asset being supplied
         @param _amount The amount of the asset being supplied
+        @param _comptroller The address of Compound's Comptroller
     */
     function deposit(
         address _underlying,
         uint256 _amount,
         address _comptroller
-    ) internal {
+    ) external {
         require(_amount > 0, "CompoundPoolController: Amount must be greater than 0");
 
         address cErc20Contract = getCErc20Contract(_underlying);
@@ -67,15 +56,73 @@ library CompoundPoolController {
         @param _underlying The address of the underlying asset being borrowed
         @param _amount The amount of the underlying asset being supplied
     */
-    function borrow(
-        address _underlying,
-        uint256 _amount
-    ) internal {
+    function borrow(address _underlying, uint256 _amount) external {
         require(_amount > 0, "CompoundPoolController: Amount must be greater than 0");
 
         address cErc20Contract = getCErc20Contract(_underlying);
         CErc20 cToken = CErc20(cErc20Contract);
 
+        //Borrow Tokens
         cToken.borrow(_amount);
+    }
+
+    /**
+        @dev Get the maximum borrow amount for a specific currency
+        @param _underlyingCollateral The address of the underlying token that serves as collateral
+        @param _amount The amount of collateral that the user has deposited
+        @param _underlyingBorrow The address of the token to be borrowed
+        @param _comptroller The address of Compound's comptroller
+        @param _priceFeed The address of Compound's price feed
+    */
+    function getMaxUnderlyingBorrowAmount(
+        address _underlyingCollateral,
+        uint256 _amount,
+        address _underlyingBorrow,
+        address _comptroller,
+        address _priceFeed
+    ) public returns (uint256) {
+        address collateralCErc20Contract = getCErc20Contract(_underlyingCollateral);
+        CErc20 collateral = CErc20(collateralCErc20Contract);
+        CErc20 borrowing = CErc20(getCErc20Contract(_underlyingBorrow));
+        Comptroller comptroller = Comptroller(_comptroller);
+        PriceFeed priceFeed = PriceFeed(_priceFeed);
+
+        // Get the collateral factor of the collateralized asset
+        (, uint256 collateralFactorMantissa) =
+            comptroller.markets(collateralCErc20Contract);
+
+        // Calculate the USD Balance of the user
+        uint256 underlyingBalanceMantissa = _amount
+            .mul(collateral.exchangeRateCurrent())
+            .div(1e18);
+
+        uint256 usdBalanceMantissa = underlyingBalanceMantissa
+            .mul(priceFeed.getUnderlyingPrice(collateralCErc20Contract))
+            .div(1e18);
+
+        // Calculate the total USD borrow amount
+        uint256 usdBorrowAmountMantissa = usdBalanceMantissa
+            .mul(collateralFactorMantissa)
+            .div(1e18);
+
+        // Return the total borrow supply for the asset
+        return usdBorrowAmountMantissa
+            .mul(priceFeed.getUnderlyingPrice(getCErc20Contract(_underlyingBorrow)))
+            .div(1e18);
+    }
+
+    /**
+        @dev Returns a token's cToken contract address given its ERC20 contract address.
+        @param _erc20Contract The ERC20 contract address of the token
+    */
+    function getCErc20Contract(address _erc20Contract) private pure returns (address) {
+        if (_erc20Contract == 0x0D8775F648430679A709E98d2b0Cb6250d2887EF) return 0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E; // BAT => cBAT
+        if (_erc20Contract == 0xc00e94Cb662C3520282E6f5717214004A7f26888) return 0x70e36f6BF80a52b3B46b3aF8e106CC0ed743E8e4; // COMP => cCOMP
+        if (_erc20Contract == 0x6B175474E89094C44Da98b954EedeAC495271d0F) return 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643; // DAI => cDAI
+        if (_erc20Contract == 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984) return 0x35A18000230DA775CAc24873d00Ff85BccdeD550; // UNI => cUNI
+        if (_erc20Contract == 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48) return 0x39AA39c021dfbaE8faC545936693aC917d5E7563; // USDC => cUSDC
+        if (_erc20Contract == 0xdAC17F958D2ee523a2206206994597C13D831ec7) return 0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9; // USDT => cUSDT
+        if (_erc20Contract == 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599) return 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4; // WBTC => cWBTC
+        else revert("CompoundFundController: Supported Compound cToken address not found for this token address");
     }
 }
