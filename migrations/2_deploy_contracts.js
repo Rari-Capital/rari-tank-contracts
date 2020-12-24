91f037aa62adc20a9da53f0287565e781364faef
Jet <jetjadeja@gmail.com>
2020-12-23 16:14:14 -0800

    🚀  Added Rebalancer to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,20 +6,26 @@ const RariFundController = artifacts.require("RariFundController");
 const CompoundPoolController = artifacts.require("CompoundPoolController");
 
 module.exports = async (deployer, network, accounts) => {
-  const owner = accounts[0];
+  const [owner, rebalancer] = accounts;
+
+  //Address with the least slippage in the Rari Stable Pool
+  const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+
   //Deploy the RariFundManager and RariFundController
   await deployer.link(CompoundPoolController, RariFundController);
   await deployer.deploy(RariFundManager, { from: owner }).then(async () => {
     await deployer
       .deploy(RariFundController, { from: owner })
       .then(async () => {
-        console.log(RariFundController.address);
-
         const managerInstance = await RariFundManager.deployed();
         const controllerInstance = await RariFundController.deployed();
 
         managerInstance.initialize(RariFundController.address);
-        controllerInstance.initialize(RariFundManager.address);
+        controllerInstance.initialize(
+          RariFundManager.address,
+          rebalancer,
+          USDC
+        );
 
         tokens.map((token) => {
           controllerInstance.newTank(
