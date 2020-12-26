47d848c431c5b719cf56b7fe9b2e1d19cfeacb94
JetDeveloping <jetjadeja@gmail.com>
2020-12-26 11:42:45 -0700

    🎨 Setup FundController for deployment

--- contracts/RariFundController.sol
+++ contracts/RariFundController.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.7.0;
 
 contract RariFundController {
+    address private rariFundManager;
+
+    constructor(address _rariFundManager) {
+        rariFundManager = _rariFundManager;
+    }
+
     function deposit(address account, uint256 amount) external {}
 }
