e2b0ed7f6fbc899258ebe2ab2de236aac47f9a48
JetDeveloping <jetjadeja@gmail.com>
2020-12-27 16:01:45 -0700

    🚀 Finished writing migrations

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,39 @@
+import { Func } from "mocha";
+
 const hre = require("hardhat");
+const ethers = hre.ethers;
+
+const tokens: Array<String> = require("./helpers/tokens");
+const contracts = require("./helpers/contracts");
 
 async function deploy() {
+  const [owner, rebalancer] = await ethers.getSigners();
+
   //prettier-ignore
-  const RariFundManager = await hre.ethers.getContractFactory("RariFundManager");
+  const RariFundManager = await ethers.getContractFactory("RariFundManager");
   //prettier-ignore
-  const RariFundController = await hre.ethers.getContractFactory("RariFundController");
+  const RariFundController = await ethers.getContractFactory("RariFundController");
 
   const rariFundManager = await RariFundManager.deploy();
   await rariFundManager.deployed();
 
-  //prettier-ignore
-  const rariFundController = await RariFundController.deploy(rariFundManager.address);
+  const rariFundController = await RariFundController.deploy(
+    rariFundManager.address,
+    rebalancer.address,
+    contracts.comptroller,
+    contracts.priceFeed
+  );
   await rariFundController.deployed();
-
   rariFundManager.setRariFundController(rariFundController.address);
 
-  console.log(rariFundManager.address);
+  console.log(await rariFundController.owner());
+  console.log(owner.address);
+
+  for (let i = 0; i < tokens.length; i++) {
+    await rariFundController
+      .newTank(tokens[i])
+      .catch((error: any) => console.log(error));
+  }
 }
 
 deploy()
