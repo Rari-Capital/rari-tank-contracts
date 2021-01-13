a8a0b21b19959996ad01b5528e93b0276c2ecf87
JetDeveloping <jetjadeja@gmail.com>
2021-01-12 23:31:18 -0800

    📦  Setup Coverage for testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    hardhat: {
+      forking: {
+        url:
+          "https://eth-mainnet.alchemyapi.io/v2/UPMBuJ4TAQrsy9sdb4QSKuanqG1EYR3L",
+        blockNumber: 11635038,
+      },
+    },
+  },
   solidity: "0.7.3",
 };
