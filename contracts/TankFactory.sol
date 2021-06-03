4358a5b037e58b86561accae43ba736e51142a8e
JetDeveloping <jetjadeja@gmail.com>
2021-06-02 22:22:52 -0700

    Rename TankFactory

--- contracts/TankFactory.sol
+++ contracts/TankFactory.sol
@@ -1,10 +1,13 @@
 pragma solidity 0.7.3;
 
+/* Storage */
+import {TankFactoryStorage} from "./factory/TankFactoryStorage.sol";
+
 /**
     @title TankFactory
     @author Jet Jadeja <jet@rari.capital>
     @dev Manages Tank deployments, new strategies, and rebalances
 */
-contract TankFactory {
+contract TankFactory is TankFactoryStorage {
 
 }
